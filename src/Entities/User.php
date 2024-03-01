0a184e8081947a9418b54cd95387494a1d11ccf2
Ashley Coles <ashley@acoles.co.uk>
2024-03-01 11:37:32 +0000

    Entity tweaks for nullable fields, and model methods

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -2,12 +2,12 @@
 
 readonly class User
 {
-    public int $id;
+    public ?int $id;
     public string $username;
     public string $password;
-    public string $bio;
+    public ?string $bio;
 
-    public function __construct(int $id, string $username, string $password, string $bio)
+    public function __construct(string $username, string $password, ?string $bio = null, ?int $id = null)
     {
         $this->id = $id;
         $this->username = $username;
--- src/Models/UserModel.php
+++ src/Models/UserModel.php
@@ -1,6 +1,76 @@
 <?php
 
+require_once 'src/Entities/User.php';
+
 class UserModel
 {
+    private PDO $db;
 
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function getById(int $id): User|false
+    {
+        $query = $this->db->prepare('SELECT `id`, `username`, `password`, `bio` FROM `users` WHERE `id` = :id;');
+        $query->execute([
+            ':id' => $id
+        ]);
+
+        $data = $query->fetch();
+
+        if (!$data) {
+            return false;
+        }
+
+        return $this->hydrate($data);
+    }
+
+    public function getByUsername(string $username): User|false
+    {
+        $query = $this->db->prepare('SELECT `id`, `username`, `password`, `bio` FROM `users` WHERE `username` = :username;');
+        $query->execute([
+            ':username' => $username
+        ]);
+
+        $data = $query->fetch();
+
+        if (!$data) {
+            return false;
+        }
+
+        return $this->hydrate($data);
+    }
+
+    public function usernameIsUnique(string $username): bool
+    {
+        $query = $this->db->prepare('SELECT `id` FROM `users` WHERE `username` = :username;');
+
+        $query->execute([
+            ':username' => $username
+        ]);
+
+        $result = $query->fetch();
+
+        if (!$result) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function addUser(User $user): bool
+    {
+        $query = $this->db->prepare('INSERT INTO `users` (`username`, `password`, `bio`) VALUES (:username, :password, :bio)');
+        return $query->execute([
+            ':username' => $user->username,
+            ':password' => $user->password,
+            ':bio' => $user->bio
+        ]);
+    }
+    private function hydrate(array $data): User
+    {
+        return new User($data['username'], $data['password'], $data['bio'], $data['id']);
+    }
 }
